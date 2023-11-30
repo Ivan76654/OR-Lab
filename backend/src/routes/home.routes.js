3840aed3411ebfc958598eafe3bfec6b34437380
Ivan76654 <ivan.lovric2101@gmail.com>
2023-11-30 16:35:37 +0100

    Add filter endpoint for filtering the data and refactor code.

--- backend/src/routes/home.routes.js
+++ backend/src/routes/home.routes.js
@@ -1,34 +1,102 @@
 const express = require('express');
+const pgFormat = require('pg-format');
 const { query } = require('../db');
 const { format } = require('date-fns');
 
 const homeRouter = express.Router();
 
-homeRouter.get('/', (req, res) => {
-	(async () =>{
-		const sqlQuery = `SELECT leagues.*, teams.*, players.* FROM leagues NATURAL JOIN teams NATURAL JOIN players;`;
+function formatDatesInResultSet(rows, dateFormat) {
+	return rows.map((row) => {
+		return {
+			...row,
+			founded: format(row.founded, dateFormat),
+			date_of_birth: format(row.date_of_birth, dateFormat),
+		};
+	});
+}
 
+homeRouter.get('/', (req, res) => {
+	(async () => {
 		try {
+			const sqlQuery = `SELECT leagues.*, teams.*, players.* FROM leagues NATURAL JOIN teams NATURAL JOIN players;`;
 			const result = await query(sqlQuery, []);
 
-			const formattedResult = result.rows.map(row => {
-				return {
-					...row,
-					founded: format(row.founded, 'dd.MM.yyyy'),
-					date_of_birth: format(row.date_of_birth, 'dd.MM.yyyy')
-				}
-			});
+			const formattedResult = formatDatesInResultSet(result.rows, 'yyyy-MM-dd');
 
 			res.json({
 				rowCount: result.rowCount,
-				data: formattedResult
+				data: formattedResult,
 			});
-
-		} catch(err) {
+		} catch (err) {
 			console.log(err);
 			res.sendStatus(500);
 		}
+	})();
+});
 
+homeRouter.post('/filter', (req, res) => {
+	(async () => {
+		try {
+			const filterField = req.body.filterField;
+			const filterValue = req.body.filterValue;
+
+			let sqlQuery = 'SELECT leagues.*, teams.*, players.* FROM leagues NATURAL JOIN teams NATURAL JOIN players';
+			let params = [];
+
+			switch (filterField) {
+				case '*':
+					const allColumns = [
+						'league_id',
+						'league_rank',
+						'number_of_teams',
+						'team_id',
+						'team_name',
+						'founded',
+						'player_id',
+						'player_first_name',
+						'player_last_name',
+						'date_of_birth',
+						'elo_rank',
+					];
+
+					let filterHelper = 'WHERE';
+
+					for (let i = 0; i < allColumns.length; i++) {
+						if (i === allColumns.length - 1) {
+							filterHelper += ` LOWER(${allColumns[i]}::VARCHAR) = LOWER($1)`;
+
+							continue;
+						}
+
+						filterHelper += ` LOWER(${allColumns[i]}::VARCHAR) = LOWER($1) OR`;
+					}
+
+					params.push(filterValue);
+					sqlQuery = `${sqlQuery} ${filterHelper};`;
+					break;
+
+				default:
+					sqlQuery = pgFormat(
+						`${sqlQuery} WHERE LOWER(%I::VARCHAR) = LOWER(%L);`,
+						filterField,
+						filterValue
+					);
+					break;
+			}
+
+			const result = await query(sqlQuery, params);
+
+			const formattedResult = formatDatesInResultSet(result.rows, 'yyyy-MM-dd');
+
+			res.json({
+				rowCount: result.rowCount,
+				data: formattedResult,
+			});
+			
+		} catch (err) {
+			console.log(err);
+			res.sendStatus(500);
+		}
 	})();
 });
 
